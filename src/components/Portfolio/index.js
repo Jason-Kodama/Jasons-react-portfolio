8e6761fc45bcec29a2702523bc2b578a2edeaf8a
Jason <jasonkodama@gmail.com>
2023-09-20 11:53:30 -0600

    added description to projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,36 +4,42 @@ import Project from '../Project';
 function Portfolio() {
     const [projects] = useState([
         {
-          name: 'text-editor',
-          description: 'PWA',
-          link: "https://jasons-pwa-texteditor-18ac94c046a1.herokuapp.com/",
-          repo: "https://github.com/Jason-Kodama/Jasons-PWA-textEditor"
+          name: 'book-search',
+          description: 'A book search application using the MERN stack.',
+          link: "https://fathomless-badlands-64324-acf18859dc40.herokuapp.com/",
+          repo: "https://github.com/Jason-Kodama/Jasons-book-search"
           
         },
         {
           name: 'noSql-api',
-          description: 'MongoDB',
+          description: 'Using noSQL, this is a social network API that allows a user to create and share a thought and react to another friends thought.',
           link: "https://drive.google.com/file/d/1FJK7t4heKG4PqMkXfpsYqMv6__joQlzJ/view",
           repo: "https://github.com/Jason-Kodama/Jasons-noSql-api"
         },
         {
           name: 'note-taker',
-          description: 'express',
+          description: 'A note taking application using Express.js',
           link: "https://protected-harbor-16972.herokuapp.com/",
           repo: "https://github.com/Jason-Kodama/Jasons-express-notetaker"
         },
         {
           name: 'Chat Camp',
-          description: 'Node/Express/mySql',
+          description: 'A chat application that allows users to login and chat with fellow bootcamp students. Also has ChatGPT integrated to ask questions using AI.',
           link: "https://chat-camp.herokuapp.com/",
           repo: "https://github.com/Jason-Kodama/Portfolio-02"
         },
         {
           name: 'Boss-Fight',
-          description: 'HTML/CSS/JavaScript',
+          description: 'An application that allows a user to pick the winner of a fight between video game bosses.',
           link: "https://bizthehabesha.github.io/group-6-project-1/",
           repo: "https://github.com/Jason-Kodama/group-6-project-1"
         },
+        {
+          name: 'The-Magic-Carpet',
+          description: "A travel application that allows users to search for flights using MERN stack.",
+          link: "https://the-magic-carpet-037d75dd3e3d.herokuapp.com/",
+          repo: "https://github.com/Jason-Kodama/The-Magic-Carpet"
+        }
       ]);
 
       return (
